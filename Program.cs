09caa7a0e6baa5d2f4e5be352572aeeaee2e8623
antonsefwerwerww <filipenko.anton1123@vu.cdu.edu.ua>
2023-11-15 13:18:26 +0200

    Створено консольний додаток

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Program
+{
+    class Public
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
